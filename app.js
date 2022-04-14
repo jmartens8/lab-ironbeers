795a679bcc2269cffcb06dbf80029b451f1eed17
Jonathan Martens <jmartens8@yahoo.de>
2022-04-14 12:30:18 +0200

    iteration 4.1 stable

--- app.js
+++ app.js
@@ -12,14 +12,39 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 // Register the location for handlebars partials here:
+
 
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index');  
 });
+
+app.get('/beers', function(req, res){
+  
+  punkAPI
+  .getBeers()
+  .then(beersFromApi => {
+    res.render('beers', {beersList: beersFromApi});
+
+    console.log('Beers from the database: ', beersFromApi)
+  })
+  .catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req, res) => {
+
+  punkAPI
+  .getRandom()
+  .then(responseFromAPI => {
+    res.render('random-beer', {rendomBeer: responseFromAPI});
+  })
+  .catch(error => console.log(error));
+})
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
